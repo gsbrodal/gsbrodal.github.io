26711dfa5c938f2c0b084f72558dd5df420cda07
Gerth Stølting Brodal <gerth@cs.au.dk>
2026-04-11 09:03:48 +0200

    exam-question-dk-en.py exam-rules.md

---
+++ ipsa/exam-question-dk-en.py
@@ -0,0 +1,87 @@
+'''
+    MAKE DISTINCT
+
+#> DK
+    [DANSK PROBLEMFORMULERING]
+
+    Din opgave er, givet en liste af heltal, at erstatte gentagne forekomster af
+    heltal med de mindst mulige større heltal, for at gøre alle heltal unikke. 
+    Den resulterende liste skal udskrives sorteret i stigende rækkefølge.
+
+    Eksempel: Betragt input-sekvensen
+
+      10 6 10 4 2 2 6 2 2
+        
+    Sekvensen indeholder 4 forekomster af 2, 2 forekomster af 6 og 2
+    forekomster af 10, dvs. at 3 forekomster af 2, 1 forekomst af 6 og 1
+    forekomst af 10 skal erstattes af større heltal. 2-tallene kan
+    erstattes med 3, 5 og 7 (da listen allerede indeholder 4 og 6), 6-tallet kan
+    erstattes med 8 (da 7 allerede er indsat), og én forekomst af
+    10 kan erstattes med 11. Hvis vi sorterer den endelige liste, får vi:
+
+      2 3 4 5 6 7 8 10 11          
+
+    Input:  En enkelt linje med n mellemrumsseparerede heltal, hvor 
+            1 <= n <= 1000 og alle heltal er mellem 1 og 1_000_000_000.
+
+    Output: En enkelt linje med n mellemrumsseparerede unikke heltal, sorteret i 
+            stigende rækkefølge, hvor gentagne heltal i inputtet er erstattet
+            med de mindst mulige større heltal.
+
+    Eksempel:
+
+      Input:  10 6 10 4 2 2 6 2 2
+
+      Output: 2 3 4 5 6 7 8 10 11
+
+
+    [ENGLISH PROBLEM STATEMENT]
+
+#< DK
+    Your task is given a list of integers, to replace repeating occurrences of
+    integers with the smallest possible larger integers, to make all integers
+    distinct. The resulting list should be printed sorted in increasing order.
+
+    Example: Consider the input sequence
+
+      10 6 10 4 2 2 6 2 2
+        
+    The sequence contains 4 occurrences of 2, 2 occurrences of 6, and 2
+    occurrences of 10, i.e., 3 occurrences of 2, 1 occurrence of 6, and 1
+    occurrence of 10 need to be replaced by larger integers. The 2's can be
+    replaced by 3, 5, and 7 (since the list already contains 4 and 6), the 6 can
+    be replaced by 8 (since 7 already has been inserted), and one occurrence of
+    10 can be replaced by 11. If we sort the final list, we get:
+
+      2 3 4 5 6 7 8 10 11          
+
+    Input:  A single line of n space-separated integers, where 1 <= n <= 1000  
+            and all integers between 1 and 1_000_000_000.
+
+    Output: A single line of n space-separated distinct integers, sorted in 
+            increasing order, where recurring integers in the input are replaced
+            with the smallest possible larger integers.
+
+    Example:
+
+      Input:  10 6 10 4 2 2 6 2 2
+
+      Output: 2 3 4 5 6 7 8 10 11
+'''
+
+
+# insert code
+pass
+#> solution
+L = list(map(int, input().split()))
+#< solution
+#> validate input
+assert 1 <= len(L) <= 1000
+assert all(1 <= x <= 1_000_000_000 for x in L)
+#< validate input
+#> solution
+distinct = []
+for x in sorted(L):
+    distinct.append(x if not distinct or x > distinct[-1] else distinct[-1] + 1)
+print(*distinct)
+#< solution
--- ipsa/exam-rules.md
+++ ipsa/exam-rules.md
@@ -18,7 +18,9 @@ At most one screen is allowed, max 17 inches (see §4,
 
 _Accessing the internet is not allowed during the exam, except for downloading the exam questions, submitting your exam answers, and for running the WISEflow Device Monitor._
 
-_It is exam cheating, if you use an AI assistant for the exam (ChatGPT, GitHub Copilot, etc.). Locally installed AI assistants on your computer are also not allowed._
+_It is exam cheating, if you use an AI assistant for the exam (ChatGPT, GitHub Copilot, etc.). Locally installed AI assistants on your computer are also not allowed.
+
+_**New for the exam June 2026**: The exam statements and the [`__`README`__`.txt](__README__.txt) file will be both in Danish and English ([example](exam-question-dk-en.py), originally Problem G, exam August 2025). Searching the internet is **not allowed** during the exam (using AI assistants are still not allowed, neither online or locally on your computer)._
 
 ## WISEflow and Linux
 
